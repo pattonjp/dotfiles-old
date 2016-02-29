1c4b445d2bfe62f98bed253665bae61e1e866281
jeremy <pattonjp@gmail.com>
2016-02-29 14:14:34 -0600

    updated docker plugin

--- oh-my-zsh/custom/plugins/docker/docker.plugin.zsh
+++ oh-my-zsh/custom/plugins/docker/docker.plugin.zsh
@@ -16,16 +16,35 @@ docker-delete-all-stopped-containers(){
   docker rm $(docker ps -a -q)
 }
 
+docker-clean() {
+  docker ps -a -q -f status=exited | xargs -n 10 docker rm -v
+  docker rmi $(docker images -f "dangling=true" -q)
+  docker volume rm $(docker volume ls -qf dangling=true)
+
+}
+
 docker-machine-create(){
+  echo "Creating machine $1"
   docker-machine create -d virtualbox \
     --virtualbox-cpu-count "4" \
     --virtualbox-disk-size "30000" \
     --virtualbox-memory "3072" \
     --engine-insecure-registry dockerregistry:8085 \
+    --engine-opt dns=8.8.8.8 \
+    --engine-opt dns=8.8.4.4 \
+    --engine-opt dns-search=service.consul \
     $1
-  echo $(dm ip) "dockerhost-$1" | sudo tee -a /etc/hosts
+  echo $(docker-machine ip) "dockerhost-$1" | sudo tee -a /etc/hosts
 }
 
 setMachine(){
   eval $(dm env $1)
+}
+
+dps(){
+  docker ps --format "table {{.ID}}\t{{.Status}}\t{{.Names}}\t{{.Image}} "
+}
+
+docker-ip() {
+  dm ip $1
 }
